6b228d704e054f3b43316e0847c6dec435ef7ef2
Igor <igoranchikkuzmin@yandex.ru>
2023-03-08 00:19:32 +0100

    feat(feat/websocket): add fastapi

---
+++ endpoints/pipeline.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from fastapi import Request
+class Message(BaseModel):
+    isReceived: bool
+    type: str
+    content: str
+
+pipeline_router = APIRouter(
+    prefix="/pipeline",
+    tags=["pipeline"],
+)
+
+
+
+@pipeline_router.post("/")
+async def get_msg(request: Request):
+    data = await request.json()
+    content = data["content"]
+    content["isReceived"] = True
+    return content
+
+@pipeline_router.get("/test")
+async def test():
+    return {"message": "what? do you want to ask to me"}
