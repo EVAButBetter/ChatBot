289a668b2d6f26af0e7281a0450cc13b53fe9ba9
Igor <igoranchikkuzmin@yandex.ru>
2023-03-08 21:03:25 +0100

    minor(feat/intent_class): convert functions to methods

--- intent/Intent.py
+++ intent/Intent.py
@@ -5,14 +5,14 @@ class Intent:
     def __init__(self) -> None:
         pass
         
-    def get_slot():
+    def get_slot(self):
         """
         get slot value from a specific intent. provide the slot_name, return a tuple contains all the details
         
         """
         
     
-    def fill_slot():
+    def fill_slot(self):
         """
         set value to a specific slot,provide the slot_name, slot_value
         
@@ -24,37 +24,37 @@ class Intent:
         
     #     """
         
-    def check_value():
+    def check_value(self):
         """
         check whether the <slot,value> is existed in the database
         
         """
     
-    def check_slot():
+    def check_slot(self):
         """
         check whether all the required slots have already been filled
         
         """
         
-    def request():
+    def request(self):
         """
         request information from user.
         
         """
         
-    def confirm():
+    def confirm(self):
         """
         confirm the user's input
         
         """
         
-    def inform():
+    def inform(self):
         """
         inform the user all the information about this intent
         
         """
         
-    def release():
+    def release(self):
         """
         release the resource, prepare for the next question
         
